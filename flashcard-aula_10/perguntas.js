c687a2703103d7e6f6d156417fc11686217130d6
Poliiana <00001118810016sp@al.educacao.sp.gov.br>
2024-11-13 08:44:29 -0300

    Add files via upload
    projeto final

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro "O Princípe?"',
+    'Maquíavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital d0 Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    'A resposta é 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é BYE (BAI)'
+)
